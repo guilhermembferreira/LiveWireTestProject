3b76f917e2eec4ef26dd0e3c181453d679d26a06
Guilherme Manuel Baptista Ferreira <98644391+guilhermembferreira@users.noreply.github.com>
2024-10-08 17:45:02 +0100

    big atualization

---
+++ app/Livewire/FormContact.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Contact;
+use Livewire\Component;
+use Illuminate\Support\Facades\Log;
+
+class FormContact extends Component
+{
+    public $name, $email, $phone;
+
+    public $error = "";
+    public $success = "";
+
+    public function new_contact()
+    {
+        // validação
+        $this->validate([
+            'name' => 'required|min:3|max:50',
+            'email' => 'required|email|min:5|max:50',
+            'phone' => 'required|min:5|max:20',
+        ]);
+
+        // guardar na DB
+        $result = Contact::firstOrCreate([
+            'name' => $this->name,
+            'email' => $this->email,
+        ],
+        [
+            'phone' => $this->phone
+        ]);
+
+        // verificar se houve um erro
+        if($result->wasRecentlyCreated){
+            $this->reset();
+            $this->success = "Contact created successfully!";
+        } else {
+            $this->error = "Error creating contact!";
+        }
+
+    }
+    public function render()
+    {
+        return view('livewire.form-contact');
+    }
+}
---
+++ resources/views/livewire/form-contact.blade.php
@@ -0,0 +1,41 @@
+<div class="card p-5">
+    <form wire:submit="new_contact">
+
+        <div class="mb-3">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" id="name" wire:model="name">
+            @error('name') <span class="text-danger">{{ $message }}</span> @enderror
+        </div>
+
+        <div class="mb-3">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email" wire:model="email">
+            @error('email') <span class="text-danger">{{ $message }}</span> @enderror
+        </div>
+
+        <div class="mb-3">
+            <label for="phone">Phone</label>
+            <input type="phone" class="form-control" id="phone" wire:model="phone">
+            @error('phone') <span class="text-danger">{{ $message }}</span> @enderror
+        </div>
+
+        <div class="text-end">
+            <button class="btn btn-secondary px-5">Save</button>
+        </div>
+
+        @if($error)
+        <div class="alert alert-danger mt-3" x-data="{show: true}" x-show="show"
+            x-init="setTimeout(() => show = false, 2000)">
+            {{ $error }}
+        </div>
+        @endif
+
+        @if($success)
+        <div class="alert alert-success mt-3" x-data="{show: true}" x-show="show"
+            x-init="setTimeout(() => show = false, 2000)">
+            {{ $success }}
+        </div>
+        @endif
+
+    </form>
+</div>
